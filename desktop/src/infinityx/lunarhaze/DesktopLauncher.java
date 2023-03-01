9121a6330bb3e3e8f7a3f2b556591c38513f0a30
nrebei2 <rebein002@gmail.com>
2023-02-28 21:12:07 -0500

    Initial Commit

---
+++ desktop/src/infinityx/lunarhaze/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package infinityx.lunarhaze;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import infinityx.lunarhaze.GDXRoot;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Lunar Haze");
+		new Lwjgl3Application(new GDXRoot(), config);
+	}
+}
