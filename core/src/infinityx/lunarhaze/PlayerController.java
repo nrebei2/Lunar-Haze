33c477fb8565e2dd8bf83a746205d67fed37f26f
donnysa <donnysa@hotmail.com>
2023-03-06 14:50:50 -0500

    Updated PlayerController, GameplayController

--- core/src/infinityx/lunarhaze/PlayerController.java
+++ core/src/infinityx/lunarhaze/PlayerController.java
@@ -1,5 +1,28 @@
 package infinityx.lunarhaze;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.utils.Array;
+
 public class PlayerController {
-    //TODO
-}
+
+    /** Constructs a PlayerController with keyboard control enabled.
+     *  Currently, there is no support for Xbox controller.
+     */
+    public PlayerController() {
+
+    }
+
+    /**
+     * Return the action of the player (but do not process)
+     * Actions that the player is allowed to take:
+     *      Move
+     *      Attack
+     *      Collect Trap
+     *      Use Trap
+     **/
+    public int getAction() {
+        return 0;
+    }
+
+ }
