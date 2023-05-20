5b3bbd8766846cf24189d35ac15e8e394d49fd06
nrebei2 <rebein002@gmail.com>
2023-05-20 05:54:59 -0400

    Fix background around corners

--- core/src/infinityx/lunarhaze/models/Board.java
+++ core/src/infinityx/lunarhaze/models/Board.java
@@ -249,8 +249,8 @@ public class Board {
             }
         } else {
             // Draw any out of bounds tiles close to player
-            for (int i = worldToBoardX(pos.x) - 10; i < worldToBoardX(pos.x) + 10; i++) {
-                for (int j = worldToBoardY(pos.y) - 10; j < worldToBoardY(pos.y) + 10; j++) {
+            for (int i = worldToBoardX(pos.x) - 11; i < worldToBoardX(pos.x) + 11; i++) {
+                for (int j = worldToBoardY(pos.y) - 11; j < worldToBoardY(pos.y) + 11; j++) {
                     if (!inBounds(i, j)) {
                         drawTile(i, j, canvas, pos, false);
                     }
